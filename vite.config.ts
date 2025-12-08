ec02137625ce0a55d9b436c5b425a5d8b6442bc3
Pavel Kurlykov <kurlykov@tochka.com>
2025-12-08 22:20:28 +0300

    added gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-    resolve: {
+  base: '/paulkurlykov.github.io/',
+  resolve: {
     alias: {
-      '@': '/src'
+      '@': '/src',
     },
   },
-})
+});
